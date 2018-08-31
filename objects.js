cc75bda474feabc76b1b9a75730d4dcaf264cf6e
David Hansen <david.hansen.0000@gmail.com>
2018-08-31 08:28:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -16,7 +16,7 @@ updatePlaylist(playlist, "some artist", "some song");
 function removeFromPlaylist(obj, key) {
   console.log("obj:"+Object.keys(obj));
   console.log("key:"+key);
-  ;
+  delete obj[key];
   console.log("obj after delete:"+Object.keys(obj));
   return obj;
 }
