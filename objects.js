344ad0a4388f76b8d1c46c5f98a367a37904485a
David Hansen <david.hansen.0000@gmail.com>
2018-08-31 08:29:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,22 +2,18 @@ var playlist = {
   "Old MacDonald": "had a farm"
 };
 
-console.log(playlist);
 
 function updatePlaylist(obj, key, value) {
   obj[key] = value;
   return obj;
 }
 
-console.log(playlist);
-
 updatePlaylist(playlist, "some artist", "some song");
 
 function removeFromPlaylist(obj, key) {
-  console.log("obj:"+Object.keys(obj));
-  console.log("key:"+key);
+
   delete obj[key];
-  console.log("obj after delete:"+Object.keys(obj));
+  
   return obj;
 }
 
